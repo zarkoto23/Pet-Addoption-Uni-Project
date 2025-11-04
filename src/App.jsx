84bee3319785c1bc202e3013691b69c52295c0a5
Svetozar Stoev <zarkoto23@gmail.com>
2025-11-04 12:22:42 +0200

    refactor

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,11 @@
-import { Link } from "react-router-dom"
-import { NavigationMenu } from "./components/Navigation";
 
-export default function App() {
+function App() {
+
   return (
     <>
-    <NavigationMenu>
-       <Link to="#" className="px-4 py-2 hover:text-primary transition">
-          Home
-        </Link>
-      </NavigationMenu>
-    <div className=" min-h-screen flex flex-col items-center justify-center gap-6 text-center">
-      <h1 className="text-5xl font-semibold text-primary drop-shadow-sm">
-        🐾 Осинови приятел
-      </h1>
-      
-
-      <p className="text-lg text-secondary-foreground max-w-md">
-        Помогни на животно да намери дом 💕
-      </p>
-
-   
-    </div>
-
+     
     </>
- 
-  );
+  )
 }
+
+export default App
--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,7 @@
 import { createRoot } from 'react-dom/client'
+import './index.css'
 import App from './App.jsx'
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css"; 
-import { BrowserRouter } from 'react-router-dom';
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
+createRoot(document.getElementById('root')).render(
     <App />
-    </BrowserRouter>
-);
-
-
+)
